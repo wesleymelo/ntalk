69e2d796e7763633be8abd49cea8106fa9a0dfeb
Wesley Melo <wesley.melo.pereira@gmail.com>
2015-07-02 18:59:10 -0300

    Gerenciando salas do chat

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,17 +1,41 @@
 module.exports = function(io){
 
-  var sockets = io.sockets;
+  var crypto = require('crypto')
+  , md5 = crypto.createHash('md5')
+  , sockets = io.sockets;
 
   sockets.on('connection', function (client) {
       
         var session = client.handshake.session
             , usuario = session.usuario;
       
-  		client.on('send-server', function (msg) {
-  			msg = "<b>"+usuario.nome+":</b> "+msg+"<br>";
-  			client.emit('send-client', msg);
-  			client.broadcast.emit('send-client', msg);
+  		client.on('send-server', function (msg) {            
+            var msg = "<b>"+ usuario.nome +":</b> "+ msg +"<br>";
+            client.get('sala', function(erro, sala) {
+                var data = {email: usuario.email, sala: sala};
+                client.broadcast.emit('new-message', data);
+                sockets.in(sala).emit('send-client', msg);
+            });
   		});
+      
+        client.on('join', function(sala) {
+            if(sala) {
+                sala = sala.replace('?','');
+            } else {
+                var timestamp = new Date().toString();
+                var md5 = crypto.createHash('md5');
+                sala = md5.update(timestamp).digest('hex');
+            }
+            client.set('sala', sala);
+            client.join(sala);
+        });
+      
+      
+        client.on('disconnect', function () {
+            client.get('sala', function(erro, sala) {
+                client.leave(sala);
+            });
+        });
   });
 
 }
